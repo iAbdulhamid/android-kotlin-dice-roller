2b239989581ec5b0e2b8e3669fe09d3c654c6550
Ibrahim Abdelhamed <i_abdulhamid@outlook.com>
2023-05-23 21:18:31 +0200

    Exercise: findViewById

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        var rollButton : Button = findViewById(R.id.btn_roll)
+        rollButton.setOnClickListener {
+            rollButton.text = "Roll Me!"
+        }
     }
 }
